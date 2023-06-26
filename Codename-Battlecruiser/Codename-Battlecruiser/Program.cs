5b0958dcc7cef40d5112a40c4c378502bcd899b0
-FantoMIStranniK- <synergyivanb@gmail.com>
2023-06-26 14:04:43 +0300

    Base created

---
+++ Codename-Battlecruiser/Codename-Battlecruiser/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Codename_Battlecruiser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
